448202794f31dd3c82df119d2230adfa5f3be844
alanleungcn <alanleung1909@gmail.com>
2023-01-13 01:28:18 +0800

    fix: button enabled active class

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -30,14 +30,14 @@ const Button = React.forwardRef<HTMLButtonElement, Props>(
       className={clsx(
         "flex h-10 select-none items-center justify-center gap-2 rounded-md transition-all",
         "enabled:hover:border-border-200 enabled:hover:text-fg-200",
-        "active:bg-bg-300 active:text-fg-200",
+        "enabled:active:bg-bg-300 enabled:active:text-fg-200",
         "disabled:cursor-not-allowed disabled:opacity-50",
         icon ? "w-10 flex-shrink-0" : "px-4",
         plain
           ? "h-auto w-auto border-none bg-transparent p-[2px] opacity-80 enabled:hover:opacity-100"
           : "border border-border-100 bg-bg-200 text-fg-100",
         error &&
-          "text-red-500 focus-visible:ring-red-200/80 focus-visible:ring-offset-red-400 active:text-red-600 enabled:hover:text-red-600 dark:focus-visible:ring-red-400/20 dark:focus-visible:ring-offset-red-600",
+          "text-red-500 focus-visible:ring-red-200/80 focus-visible:ring-offset-red-400 enabled:hover:text-red-600 enabled:active:text-red-600 dark:focus-visible:ring-red-400/20 dark:focus-visible:ring-offset-red-600",
         fullWidth && "w-full",
         className,
       )}
