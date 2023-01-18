c2b4bbb774598f92ce04548215d4c1f9fc78be50
alanleungcn <alanleung1909@gmail.com>
2023-01-18 20:35:23 +0800

    fix: navigation link underline transition

--- src/components/ui/NavLink.tsx
+++ src/components/ui/NavLink.tsx
@@ -29,7 +29,7 @@ const NavLink = React.forwardRef<HTMLAnchorElement, Props>(
         icon && inMenu ? "gap-2" : icon && "gap-[6px]",
         inMenu
           ? "border-b border-border-100 px-4 py-2 first:rounded-t-md last:rounded-b-md last:border-b-0 hover:bg-bg-300/50 hover:ring-0 hover:ring-offset-0 active:bg-bg-300"
-          : `shadow-[0_2px_transparent] transition-shadow hover:shadow-[0_2px_rgb(var(--brand))]`,
+          : `shadow-[0_2px_transparent] transition-[box-shadow,color] hover:shadow-[0_2px_rgb(var(--brand))]`,
       )}
     >
       {icon}
