0c63af7c78fbc4f5bcd1119ece7b7e8017523b9b
alanleungcn <alanleung1909@gmail.com>
2023-01-28 13:18:24 +0800

    fix: email rate limiting

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,4 +1,3 @@
-// Prisma adapter for NextAuth, optional and can be removed
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import NextAuth, { type NextAuthOptions } from "next-auth";
 import type { SendVerificationRequestParams } from "next-auth/providers/email";
@@ -7,8 +6,26 @@ import { createTransport } from "nodemailer";
 import { env } from "../../../env/server.mjs";
 import { prisma } from "../../../server/db/client";
 
+// TODO: find a better solution for rate limiting
+// Since there's no way to pass around states,
+// we need to call this function 2 times to prevent sending email and creating database record.
+// https://github.com/nextauthjs/next-auth/blob/6132c3fa751b647dd8261ac9b30e24fb140a2f1a/packages/next-auth/src/core/lib/email/signin.ts
+async function rateLimited(identifier: string, expires: Date) {
+  const count = await prisma.verificationToken.count({
+    where: {
+      identifier: { equals: identifier },
+      AND: [
+        { expires: { gt: new Date(expires.getTime() - 60 * 1000) } },
+        { expires: { lt: expires } },
+      ],
+    },
+  });
+  console.log("request within last minute: ", identifier, count);
+
+  return count > 1;
+}
+
 export const authOptions: NextAuthOptions = {
-  // Include user.id on session
   callbacks: {
     session({ session, user }) {
       if (session.user) {
@@ -17,8 +34,18 @@ export const authOptions: NextAuthOptions = {
       return session;
     },
   },
-  // Configure one or more authentication providers
-  adapter: PrismaAdapter(prisma),
+  adapter: {
+    ...PrismaAdapter(prisma),
+    // https://github.com/nextauthjs/next-auth/blob/6132c3fa751b647dd8261ac9b30e24fb140a2f1a/packages/adapter-prisma/src/index.ts
+    async createVerificationToken(data) {
+      if (await rateLimited(data.identifier, data.expires)) return;
+
+      const verificationToken = await prisma.verificationToken.create({ data });
+      // @ts-expect-errors // MongoDB needs an ID, but we don't
+      if (verificationToken.id) delete verificationToken.id;
+      return verificationToken;
+    },
+  },
   providers: [
     EmailProvider({
       server: env.EMAIL_SERVER,
@@ -43,7 +70,11 @@ async function sendVerificationRequest({
   provider,
   identifier,
   url,
+  expires,
 }: SendVerificationRequestParams) {
+  if (await rateLimited(identifier, expires))
+    throw new Error(`Rate limit exceeded for ${identifier}`);
+
   const transport = createTransport(provider.server);
   const result = await transport.sendMail({
     to: identifier,
