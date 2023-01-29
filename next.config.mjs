11405b5ec6940b17dcf244c6d524c7f696f93080
alanleungcn <alanleung1909@gmail.com>
2023-01-30 00:34:51 +0800

    chore: add build analyzer

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,15 @@
+import bundleAnalyzer from "@next/bundle-analyzer";
+
 // @ts-check
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
  * This is especially useful for Docker builds.
  */
 !process.env.SKIP_ENV_VALIDATION && (await import("./src/env/server.mjs"));
+
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+});
 
 /** @type {import("next").NextConfig} */
 const config = {
@@ -14,4 +20,4 @@ const config = {
     defaultLocale: "en",
   },
 };
-export default config;
+export default withBundleAnalyzer(config);
