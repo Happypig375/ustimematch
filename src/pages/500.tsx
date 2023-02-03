edc15de604179b1ed0e11cac0646b2345ad6b658
alanleungcn <alanleung1909@gmail.com>
2023-02-04 01:38:16 +0800

    feat: custom 500 page

--- src/pages/500.tsx
+++ src/pages/500.tsx
@@ -1,22 +1,22 @@
 import * as Separator from "@radix-ui/react-separator";
 import Header from "@components/Header";
 
-const Custom404 = () => {
+const Custom500 = () => {
   return (
     <Header>
       <div className="flex w-full items-center justify-center">
         <div className="flex h-8 items-center gap-3">
-          <span className="text-xl font-bold">404</span>
+          <span className="text-xl font-bold">500</span>
           <Separator.Root
             decorative
             orientation="vertical"
             className="h-full w-[1.5px] bg-fg-200"
           />
-          <span className="text-sm">This page could not be found.</span>
+          <span className="text-sm">Internal Server Error.</span>
         </div>
       </div>
     </Header>
   );
 };
 
-export default Custom404;
+export default Custom500;
