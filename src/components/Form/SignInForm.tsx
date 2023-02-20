ee82cb4467d11150eeef23ded2b9542ead619e7c
alanleungcn <alanleung1909@gmail.com>
2023-02-20 17:33:34 +0800

    refactor: sign in form styles

--- src/components/Form/SignInForm.tsx
+++ src/components/Form/SignInForm.tsx
@@ -2,6 +2,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { Turnstile } from "@marsidev/react-turnstile";
 import * as Separator from "@radix-ui/react-separator";
 import { IconAlertCircle, IconMail } from "@tabler/icons-react";
+import clsx from "clsx";
 import { signIn } from "next-auth/react";
 import { useTheme } from "next-themes";
 import Head from "next/head";
@@ -46,7 +47,7 @@ const SignInForm = () => {
   const onSubmit: SubmitHandler<SignInForm> = async ({ email }) => {
     if (!cfTurnstileResponse) return;
 
-    // Clear previous query params if there're any errors
+    // Clear previous query params in case there are any errors
     router.replace("/auth/signin");
     setIsEmailLoading(true);
 
@@ -70,7 +71,7 @@ const SignInForm = () => {
 
   return (
     <>
-      {/* A workaround for input zoom on safari, only applying to this page. */}
+      {/* Safari input zoom workaround, specifically applied to this page. */}
       {/* https://stackoverflow.com/questions/4389932/how-do-you-disable-viewport-zooming-on-mobile-safari */}
       <Head>
         {typeof navigator !== "undefined" &&
@@ -81,78 +82,99 @@ const SignInForm = () => {
             />
           )}
       </Head>
-      <form
-        data-cy="sign-in-form"
-        className="flex flex-col gap-4"
-        onSubmit={handleSubmit(onSubmit)}
-      >
-        <Input
-          id="email"
-          label="Email"
-          inputMode="email"
-          {...register("email")}
-          error={errors.email?.message}
-          disabled={isEmailLoading || isGoogleLoading}
-          tips="A sign in link will be sent to you via email."
-        />
 
-        <Button
-          fullWidth
-          type="submit"
-          className="text-base"
-          loading={isEmailLoading}
-          disabled={isEmailLoading || isGoogleLoading || !cfTurnstileResponse}
+      <div className="grid w-full place-items-center overflow-auto p-6">
+        <div
+          className={clsx(
+            "prose prose-sm w-[80vw]",
+            "sm:w-[400px] sm:prose-base",
+            "dark:prose-invert",
+          )}
         >
-          <IconMail strokeWidth={1.75} className="h-5 w-5" />
-          Sign in with email
-        </Button>
+          <h2 className="!mb-12">Sign in to USTimematch</h2>
 
-        <Turnstile
-          onSuccess={setCfTurnstileResponse}
-          onError={() => setCfTurnstileResponse("")}
-          onExpire={() => setCfTurnstileResponse("")}
-          siteKey={env.NEXT_PUBLIC_CLOUDFLARE_TURNSTILE_SITE_KEY}
-          options={{ theme: theme === "light" ? "light" : "dark" }}
-        />
-      </form>
+          <form
+            data-cy="sign-in-form"
+            className="flex flex-col gap-4"
+            onSubmit={handleSubmit(onSubmit)}
+          >
+            <Input
+              id="email"
+              label="Email"
+              inputMode="email"
+              {...register("email")}
+              error={errors.email?.message}
+              disabled={isEmailLoading || isGoogleLoading}
+              tips="A sign in link will be sent to you via email."
+            />
 
-      <div className="my-8 flex items-center gap-4">
-        <Separator.Root
-          decorative
-          orientation="horizontal"
-          className="h-[1px] w-full bg-border-200"
-        />
-        <span className="text-sm">OR</span>
-        <Separator.Root
-          decorative
-          orientation="horizontal"
-          className="h-[1px] w-full bg-border-200"
-        />
-      </div>
+            <Button
+              fullWidth
+              type="submit"
+              className="mb-6 text-base"
+              loading={isEmailLoading}
+              disabled={
+                isEmailLoading || isGoogleLoading || !cfTurnstileResponse
+              }
+            >
+              <IconMail strokeWidth={1.75} className="h-5 w-5" />
+              Sign in with email
+            </Button>
+          </form>
 
-      <Button
-        fullWidth
-        className="text-base"
-        onClick={onSignInGoogle}
-        loading={isGoogleLoading}
-        disabled={isEmailLoading || isGoogleLoading}
-      >
-        <IconGoogle className="h-5 w-5" />
-        Sign in with Google
-      </Button>
+          <div className="mb-6 flex items-center gap-4">
+            <Separator.Root
+              decorative
+              orientation="horizontal"
+              className="h-[1px] w-full bg-border-200"
+            />
+            <span className="text-sm">OR</span>
+            <Separator.Root
+              decorative
+              orientation="horizontal"
+              className="h-[1px] w-full bg-border-200"
+            />
+          </div>
 
-      {(isError || typeof router.query.error === "string") && (
-        <p className="!mt-8 flex items-center gap-2 text-sm text-red-500">
-          <IconAlertCircle
-            strokeWidth={1.75}
-            className="h-5 w-5 flex-shrink-0"
+          <Button
+            fullWidth
+            className="mb-6 text-base"
+            onClick={onSignInGoogle}
+            loading={isGoogleLoading}
+            disabled={isEmailLoading || isGoogleLoading || !cfTurnstileResponse}
+          >
+            <IconGoogle className="h-5 w-5" />
+            Sign in with Google
+          </Button>
+
+          <Turnstile
+            className="mb-6"
+            onSuccess={setCfTurnstileResponse}
+            onError={() => setCfTurnstileResponse("")}
+            onExpire={() => setCfTurnstileResponse("")}
+            siteKey={env.NEXT_PUBLIC_CLOUDFLARE_TURNSTILE_SITE_KEY}
+            options={{ theme: theme === "light" ? "light" : "dark" }}
           />
 
-          {router.query.error === "OAuthAccountNotLinked"
-            ? "An account is already associated with this email. Please sign in and visit the Account page to link it with your Google account."
-            : "Something went wrong, please try again later."}
-        </p>
-      )}
+          {(isError || typeof router.query.error === "string") && (
+            <p className="flex items-center gap-2 text-sm text-red-500">
+              <IconAlertCircle
+                strokeWidth={1.75}
+                className="h-5 w-5 flex-shrink-0"
+              />
+
+              {router.query.error === "OAuthAccountNotLinked"
+                ? "An account is already associated with this email. Please sign in and visit the Account page to link it with your Google account."
+                : "Something went wrong, please try again later."}
+            </p>
+          )}
+
+          <p className="!mt-12 text-sm">
+            Tips: To migrate locally saved timetables, visit the Account page
+            after signing in.
+          </p>
+        </div>
+      </div>
     </>
   );
 };
--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -1,28 +1,10 @@
-import clsx from "clsx";
 import SignInForm from "@components/Form/SignInForm";
 import Header from "@components/Header";
 
 const SignIn = () => {
   return (
     <Header>
-      <div className="grid w-full place-items-center overflow-auto p-6">
-        <div
-          className={clsx(
-            "prose prose-sm w-[80vw]",
-            "sm:w-96 sm:prose-base",
-            "dark:prose-invert",
-          )}
-        >
-          <h2 className="!mb-8">Sign in to USTimematch</h2>
-
-          <SignInForm />
-
-          <p className="!mt-8 text-sm">
-            Tips: To migrate locally saved timetables, visit the Account page
-            after signing in.
-          </p>
-        </div>
-      </div>
+      <SignInForm />
     </Header>
   );
 };
